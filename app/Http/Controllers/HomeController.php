1b07e5416e099863b2c4398147d29088b5d33c0f
Adesanwo <kazeem.adesanwo@gmail.com>
2018-08-01 18:39:43 +0100

    complete the app for cyberfleet

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class HomeController extends Controller
-{   
-    $xml = str_replace(";","", );
+{
+    public function welcome()
+    {
+        $xml = "<;;?xml version=\"1.0\" encoding=\"UTF-8\"?>;;<;;questionList>;;<;;question>;;<;;index>;;1<;;/index>;;<;;question>;;I like questions asked<;;/question>;;<;;isMultiChoice>;;1<;;/isMultiChoice>;;<;;correctOption>;;1<;;/correctOption>;;<;;options>;;<;;option>;;<;;index>;;1<;;/index>;;<;;optionText>;;Yes<;;/optionText>;;<;;/option>;;<;;option>;;<;;index>;;2<;;/index>;;<;;optionText>;;No<;;/optionText>;;<;;/option>;;<;;/options>;;<;;/question>;;<;;question>;;<;;index>;;2<;;/index>;;<;;question>;;We need to test this<;;/question>;;<;;isMultiChoice>;;1<;;/isMultiChoice>;;<;;correctOption>;;2<;;/correctOption>;;<;;options>;;<;;option>;;<;;index>;;1<;;/index>;;<;;optionText>;;No we dont<;;/optionText>;;<;;/option>;;<;;option>;;<;;index>;;2<;;/index>;;<;;optionText>;;Who cares<;;/optionText>;;<;;/option>;;<;;option>;;<;;index>;;3<;;/index>;;<;;optionText>;;Coloured Lizards<;;/optionText>;;<;;/option>;;<;;/options>;;<;;/question>;;<;;/questionList>;;";
+        $xml = str_replace(";", "", $xml);
 
-    //$read = simplexml_load_file($xml);
+        $jsonData = collect(json_decode(json_encode((array) simplexml_load_string($xml)), true));
+
+        return view('welcome', ['questions' => $jsonData]);
+    }
+
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,33 +1,69 @@
 <!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
+<html lang="{{ app()->getLocale() }}">
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+</head>
+<body>
+<div class="row">
+    <div class="col-4 offset-4">
+        <div class="card">
+            <div class="card-body">
+                <ol>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+                    @foreach($questions['question'] as $question)
+                        <li>
+                            {{ $question['question'] }}
+                            {{ $question['correctOption'] }}
+                            @foreach($question['options']['option'] as $index => $option)
+                            <div class="form-check">
+                                <input type="radio" id="{{ $question['index'] . "_" . $option['index']  }}" name="Q{{ $question['index'] }}" class="{{ $question['correctOption'] == (int) $option['index'] ? 'correct' : 'wrong' }} form-check-input position-static optionRadio">
+                                <label class="form-check-label" for="{{ $question['index'] . "_" . $option['index']  }}">
+                                    {{ $option['optionText'] }}
+                                </label>
+                            </div>
+                            @endforeach
+                        </li>
+                    @endforeach
+                </ol>
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <div class="container">
-        <!-- Content here -->
-        <div class="row">
-            <div class="col-md-4 offset-md-4">
-                <h1>CyberFleet </h1>
+                <button class="btn btn-primary" disabled>PREVIOUS</button>
+                <button class="btn btn-primary" disabled>NEXT</button>
+                <button class="btn btn-primary" onclick="submit()">SUBMIT QUIZ</button>
+
+                <hr>
+
+                <h3 class="text-right" style="display: none" id="result">
+                    <strong>SCORE: <span class="correct"></span> out of {{ count($questions['question']) }}</strong>
+                </h3>
             </div>
         </div>
     </div>
+</div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+
+<script>
+    var score = 0;
+
+    function submit() {
+        var correct = $('.optionRadio.correct:checked').length;
+
+        $(".correct").html(correct);
+        document.getElementById("result").style.display="block";
+    }
+
+</script>
+</body>
 </html>
