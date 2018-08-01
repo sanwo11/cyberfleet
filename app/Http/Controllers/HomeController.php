df9fee718f54b1cd145780248198e958fcbb6ea5
Adesanwo <kazeem.adesanwo@gmail.com>
2018-08-01 17:43:00 +0100

    initial stage of the application

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class HomeController extends Controller
+{   
+    $xml = str_replace(";","", );
+
+    //$read = simplexml_load_file($xml);
+}
